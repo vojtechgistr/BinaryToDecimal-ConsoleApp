9b4820c8e78d89aa08d0f20b5623da5fd98fb0a9
Vojtěch Gistr <vojtikcz4@gmail.com>
2020-11-15 16:55:23 +0100

    Update Program.cs

--- source/Program.cs
+++ source/Program.cs
@@ -1,53 +1,90 @@
 using System;
-using System.Collections.Generic;
 using System.Threading;
+using System.Collections.Generic;
 
 namespace BinaryToDecimal
 {
     class Program
     {
-
         static int indexMainMenu = 0;
+
+        // checks if digit is other than 0 and 1 
+        public static bool isBinary(int number)
+        {
+            while (number > 0)
+            {
+                int digit = number % 10;
+
+             
+                if (digit > 1)
+                    return false;
+                number /= 10;
+            }
+            return true;
+        }
 
         static void Main(string[] args)
         {
         start:
             Console.ForegroundColor = ConsoleColor.White;
-            Console.WriteLine("Enter the number you want to convert:");
+            Console.WriteLine("Enter the number you want to convert (1 or 0 only):");
             Console.ForegroundColor = ConsoleColor.Cyan;
 
             int num;
 
             while (!int.TryParse(Console.ReadLine(), out num))
             {
+                
                 Console.Clear();
                 Console.ForegroundColor = ConsoleColor.Red;
                 Console.WriteLine("The input must be a number!");
                 Console.ForegroundColor = ConsoleColor.White;
                 goto start;
             }
-
-            if (num < 0)
+            
+              if (!isBinary(num))
             {
                 Console.Clear();
                 Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine("The input must be a positive number, not negative!");
+                Console.WriteLine("The input must include 1 or 0 only!");
                 Console.ForegroundColor = ConsoleColor.White;
                 goto start;
 
+            } else if (num < 0)
+            {
+                Console.Clear();
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("The input must include 1 or 0 only!");
+                Console.ForegroundColor = ConsoleColor.White;
+                goto start;
             }
             else
             {
-                string converted = Convert.ToString(num, 2);
+                int shownum = num; // stores old [entered] number
+
+                // binary value
+                int value = 0;
+
+                // initializing base value to 1 (-=- 2^0)
+                int Vbase = 1; // [Value Base]
+                while (num > 0)
+                {
+                    int token = num % 10; // modulo operation
+                    num = num / 10;
+                    value += token * Vbase;
+                    Vbase = Vbase * 2;
+                }
+
+
 
                 Console.ForegroundColor = ConsoleColor.White;
                 Console.Write("Number ");
                 Console.ForegroundColor = ConsoleColor.Cyan;
-                Console.Write(num);
+                Console.Write(shownum);
                 Console.ForegroundColor = ConsoleColor.White;
-                Console.Write(" in binary number (system) is: ");
+                Console.Write(" in decimal number (system) is: ");
                 Console.ForegroundColor = ConsoleColor.Green;
-                Console.WriteLine(converted);
+                Console.WriteLine(value);
 
                 Console.WriteLine();
                 Console.WriteLine();
@@ -79,7 +116,8 @@ namespace BinaryToDecimal
                         Console.WriteLine("Thanks for using this app");
                         Console.ForegroundColor = ConsoleColor.Cyan;
                         Console.WriteLine("- made by Vojtěch Gistr");
-                        Console.Write("- GitHub respority: https://github.com/VojtaG/BinaryToDecimal-ConsoleApp");
+                        Console.Write("- GitHub respority: https://github.com/VojtaG/Square-Root-Console-App");
+
 
                         Console.WriteLine("");
                         Console.WriteLine("");
@@ -106,81 +144,81 @@ namespace BinaryToDecimal
                     }
                 }
             }
-      }
-    //public menu selection - text
-    public static void RunMenu()
-    {
-        Console.ForegroundColor = ConsoleColor.Gray;
-        Console.WriteLine("       -------------------------------------------------------------");
-
-        Console.ForegroundColor = ConsoleColor.Blue;
-        Console.WriteLine("         Do you want to Continue or Exit? Select one of the options");
-
-        Console.ForegroundColor = ConsoleColor.Gray;
-        Console.WriteLine("       -------------------------------------------------------------");
-        Console.WriteLine();
-        Console.ForegroundColor = ConsoleColor.Gray;
-        Console.WriteLine();
-    }
-
-    // example menu code from [edited] -> https://www.codegrepper.com/code-examples/csharp/c%23+console+application+menu+system
-    //menu public function
-    public static string RunEndMenuEvent(List<string> items)
-    {
-        for (int i = 0; i < items.Count; i++)
+        }
+        //public menu selection - text
+        public static void RunMenu()
         {
-            if (i == indexMainMenu)
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.WriteLine("       -------------------------------------------------------------");
+
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("         Do you want to Continue or Exit? Select one of the options");
+
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.WriteLine("       -------------------------------------------------------------");
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.WriteLine();
+        }
+
+        // example menu code from [edited] -> https://www.codegrepper.com/code-examples/csharp/c%23+console+application+menu+system
+        //menu public function
+        public static string RunEndMenuEvent(List<string> items)
+        {
+            for (int i = 0; i < items.Count; i++)
             {
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine(items[i]);
+                if (i == indexMainMenu)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine(items[i]);
+                }
+                else
+                {
+                    Console.WriteLine(items[i]);
+
+                }
+                Console.ResetColor();
+            }
+
+            ConsoleKeyInfo ckey = Console.ReadKey();
+            if (ckey.Key == ConsoleKey.DownArrow)
+            {
+                RunMenu();
+                if (indexMainMenu == items.Count - 1) { }
+                else { indexMainMenu++; }
+            }
+            else if (ckey.Key == ConsoleKey.UpArrow)
+            {
+                RunMenu();
+                if (indexMainMenu <= 0) { }
+                else { indexMainMenu--; }
+            }
+            else if (ckey.Key == ConsoleKey.W)
+            {
+                RunMenu();
+                if (indexMainMenu <= 0) { }
+                else { indexMainMenu--; }
+            }
+            else if (ckey.Key == ConsoleKey.S)
+            {
+                RunMenu();
+                if (indexMainMenu == items.Count - 1) { }
+                else { indexMainMenu++; }
+            }
+            else if (ckey.Key == ConsoleKey.Enter)
+            {
+                Console.Clear();
+                return items[indexMainMenu];
             }
             else
             {
-                Console.WriteLine(items[i]);
-
+                Console.Clear();
+                RunMenu();
+                return "";
             }
-            Console.ResetColor();
-        }
-
-        ConsoleKeyInfo ckey = Console.ReadKey();
-        if (ckey.Key == ConsoleKey.DownArrow)
-        {
-            RunMenu();
-            if (indexMainMenu == items.Count - 1) { }
-            else { indexMainMenu++; }
-        }
-        else if (ckey.Key == ConsoleKey.UpArrow)
-        {
-            RunMenu();
-            if (indexMainMenu <= 0) { }
-            else { indexMainMenu--; }
-        }
-        else if (ckey.Key == ConsoleKey.W)
-        {
-            RunMenu();
-            if (indexMainMenu <= 0) { }
-            else { indexMainMenu--; }
-        }
-        else if (ckey.Key == ConsoleKey.S)
-        {
-            RunMenu();
-            if (indexMainMenu == items.Count - 1) { }
-            else { indexMainMenu++; }
-        }
-        else if (ckey.Key == ConsoleKey.Enter)
-        {
-            Console.Clear();
-            return items[indexMainMenu];
-        }
-        else
-        {
             Console.Clear();
             RunMenu();
             return "";
         }
-        Console.Clear();
-        RunMenu();
-        return "";
     }
-  }
 }
